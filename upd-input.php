a73670ea13d2bef02566eef23e9add732b995f09
kaimeido <kaimeido.aispeaker@gmail.com>
2019-04-09 21:25:06 +0900

    1

---
+++ upd-input.php
@@ -0,0 +1,24 @@
+<?php require '../header.php'; ?>
+
+<?php require '../footer.php'; ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+<meta name="robots" content="index">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<head>
+<meta charset="UTF-8">
+<title>コマンド配信設定ページ</title>
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+  <p>アップロードするファイルを指定してください。</p>
+  <form action="upd-output.php" method="post" enctype="multipart/form-data">
+  <p><input type="file" name="file"></p>
+  <p><input type="submit" value="アップロード"></p>
+  </form>
+
+</body>
+</html>
---
+++ upd-output.php
@@ -0,0 +1,35 @@
+<?php require '../header.php'; ?>
+<?php
+if (is_uploaded_file($_FILES['file']['tmp_name'])) {
+	if (!file_exists('upload')) {
+		mkdir('upload');
+	}
+	$file='upload/'.basename($_FILES['file']['name']);
+	if (move_uploaded_file($_FILES['file']['tmp_name'], $file)) {
+		echo $file, 'のアップロードに成功しました。';
+		echo '<p><img src="', $file, '"></p>';
+	} else {
+		echo 'アップロードに失敗しました。';
+	}
+} else {
+	echo 'ファイルを選択してください。';
+}
+?>
+<?php require '../footer.php'; ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+<meta name="robots" content="index">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<head>
+<meta charset="UTF-8">
+<title>コマンド配信設定ページ</title>
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+
+
+</body>
+</html>
