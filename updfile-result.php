fa2a257deefa5c905f52d19f2bf636e01ae4d067
kaimeido <kaimeido.aispeaker@gmail.com>
2019-04-20 22:44:17 +0900

    1

--- updfile-result.php
+++ updfile-result.php
@@ -26,8 +26,7 @@
 						<section id="cta" class="main special">
 
 						<?php
-						/*if (is_uploaded_file($_FILES['file']['tmp_name'])) {
-
+						if (is_uploaded_file($_FILES['file']['tmp_name'])) {
 							if (!file_exists('upload')) {
 								mkdir('upload');
 							}
@@ -35,69 +34,15 @@
 							$file='upload/001.jpg';
  							if (move_uploaded_file($_FILES['file']['tmp_name'], $file)) {
  								//echo $file, 'のアップロードに成功しました。';
-
-
-								//画像サイズの縮小
-								list($width, $hight) = getimagesize($file); // 元の画像名を指定してサイズを取得
-								$baseImage = imagecreatefromjpeg($file); // 元の画像から新しい画像を作る準備
-								$image = imagecreatetruecolor(500, 500); // サイズを指定して新しい画像のキャンバスを作成
-								// 画像のコピーと伸縮
-								imagecopyresampled($image, $baseImage, 0, 0, 0, 0, 500, 500, $width, $hight);
-								imagejpeg($image , $file);
-
-
  								echo '画像送信に成功しました。';
  								echo '<div id="sample"><p><img src="', $file, '"></p></div>';
  							} else {
  								echo '画像送信に失敗しました。';
  							}
-							*/
-
-
-							if (is_uploaded_file($_FILES["file"]["tmp_name"])) {
-					        $file1 = $_FILES["file"]["tmp_name"]; // 元画像ファイル
-					        $file2 = "upload/001.jpg"; // 画像保存先のパス
-					        $in = ImageCreateFromJPEG($file1); // 元画像ファイル読み込み
-					        $width = ImageSx($in); // 画像の幅を取得
-					        $height = ImageSy($in); // 画像の高さを取得
-					        $min_width = 600; // 幅の最低サイズ
-					        $min_height = 600; // 高さの最低サイズ
-					        $image_type = exif_imagetype($file1); // 画像タイプ判定用
-
-									if (!file_exists('upload')) {
-										mkdir('upload');
-									}
-
-					        if ($image_type == IMAGETYPE_JPEG){ // JPGかどうか判定
-					            if($width >= $min_width|$height >= $min_height){
-					                if($width == $height) {
-					                    $new_width = $min_width;
-					                    $new_height = $min_height;
-					                } else if($width > $height) {//横長の場合
-					                    $new_width = $min_width;
-					                    $new_height = $height*($min_width/$width);
-					                } else if($width < $height) {//縦長の場合
-					                    $new_width = $width*($min_height/$height);
-					                    $new_height = $min_height;
-					                }
-					                //　画像生成
-					                $out = ImageCreateTrueColor($new_width , $new_height);
-					                ImageCopyResampled($out, $in,0,0,0,0, $new_width, $new_height, $width, $height);
-					                ImageJPEG($out, $file2);
-
-													echo '画像送信に成功しました。';
-													echo '<div id="sample"><p><img src="', $file2, '"></p></div>';
-
-					            } else {
-					                echo "サイズが幅".$min_width."×高さ".$min_height."以上の画像をご用意ください";
-					            }
-					        } else {
-					            echo "JPG画像をご用意ください";
-					        }
-						} else {
-							echo 'ファイルを選択してください。';
 						}
 						?>
+
+						<div id="sample"><p><img src="upload/001.jpg"></p></div>
 
 						<ul class="actions special">
 						  <a href="updfile.php" class="button primary"> 戻る </a>
--- updfile.php
+++ updfile.php
@@ -44,10 +44,24 @@
 										<div id="file_input">
 											<li><input type="file" id="file" name="file"><label for="file" id="file_label">ファイルを選択</label></li>
 						        </div>
-										<li><input type="submit" class="button" value="画像送信"></li>
-
-
+										<!-- <li><input type="submit" class="button" value="画像送信1"></li> -->
 									  </form>
+
+										<!-- ファイル選択ボタン -->
+										<!--
+										<div style="width: 300px">
+											<form action="updfile-result.php" enctype="multipart/form-data" method="post">
+												<input type="file" name="userfile" accept="image/*">
+											</form>
+										</div>
+										-->
+										<!-- サムネイル表示領域 -->
+										<li><canvas id="canvas" width="0" height="0"></canvas></li>
+										<!-- アップロード開始ボタン -->
+										<!-- <li><button class="btn btn-primary" id="upload">画像送信2</button></li> -->
+										<li><button class="button" id="upload">画像送信2</button></li>
+
+
 									</ul>
 								</footer>
 							</section>
@@ -70,72 +84,108 @@
 				<script src="assets/js/main.js"></script>
 	</body>
 
-	<script type='text/javascript' src='//code.jquery.com/jquery-2.2.4.min.js'></script><script>
+
+	<!-- 以下、javascript -->
+	<script type="text/javascript">
+	$(function() {
+	  var file = null; // 選択されるファイル
+	  var blob = null; // 画像(BLOBデータ)
+	  const THUMBNAIL_WIDTH = 300; // 画像リサイズ後の横の長さの最大値
+	  const THUMBNAIL_HEIGHT = 300; // 画像リサイズ後の縦の長さの最大値
+
+	  // ファイルが選択されたら
+	  $('input[type=file]').change(function() {
+
+	    // ファイルを取得
+	    file = $(this).prop('files')[0];
+	    // 選択されたファイルが画像かどうか判定
+	    if (file.type != 'image/jpeg' && file.type != 'image/png') {
+	      // 画像でない場合は終了
+	      file = null;
+	      blob = null;
+	      return;
+	    }
+
+	    // 画像をリサイズする
+	    var image = new Image();
+	    var reader = new FileReader();
+	    reader.onload = function(e) {
+	      image.onload = function() {
+	        var width, height;
+	        if(image.width > image.height){
+	          // 横長の画像は横のサイズを指定値にあわせる
+	          var ratio = image.height/image.width;
+	          width = THUMBNAIL_WIDTH;
+	          height = THUMBNAIL_WIDTH * ratio;
+	        } else {
+	          // 縦長の画像は縦のサイズを指定値にあわせる
+	          var ratio = image.width/image.height;
+	          width = THUMBNAIL_HEIGHT * ratio;
+	          height = THUMBNAIL_HEIGHT;
+	        }
+	        // サムネ描画用canvasのサイズを上で算出した値に変更
+	        var canvas = $('#canvas')
+	                     .attr('width', width)
+	                     .attr('height', height);
+	        var ctx = canvas[0].getContext('2d');
+	        // canvasに既に描画されている画像をクリア
+	        ctx.clearRect(0,0,width,height);
+	        // canvasにサムネイルを描画
+	        ctx.drawImage(image,0,0,image.width,image.height,0,0,width,height);
+
+	        // canvasからbase64画像データを取得
+	        var base64 = canvas.get(0).toDataURL('image/jpeg');
+	        // base64からBlobデータを作成
+	        var barr, bin, i, len;
+	        bin = atob(base64.split('base64,')[1]);
+	        len = bin.length;
+	        barr = new Uint8Array(len);
+	        i = 0;
+	        while (i < len) {
+	          barr[i] = bin.charCodeAt(i);
+	          i++;
+	        }
+	        blob = new Blob([barr], {type: 'image/jpeg'});
+	        console.log(blob);
+	      }
+	      image.src = e.target.result;
+	    }
+	    reader.readAsDataURL(file);
+			$(".filename").html(file.name);
+
+	  });
 
 
-	$(function(){
-    $("#file").on('change',function(){
-      var file = $(this).prop('files')[0];
-      if(!($(".filename").length)){
-        $("#file_input").append('<span class="filename"></span>');
-      }
-      $("#file_label").addClass('changed');
-      $(".filename").html(file.name);
-    });
-  });
+	  // アップロード開始ボタンがクリックされたら
+	  $('#upload').click(function(){
 
-	var resizeImage = function(base64image, callback) {
-    const MIN_SIZE = 1000;
-    var canvas = document.createElement('canvas');
-    var ctx = canvas.getContext('2d');
-    var image = new Image();
-    image.crossOrigin = "Anonymous";
-    image.onload = function(event){
-      var dstWidth, dstHeight;
-      if (this.width > this.height) {
-        dstWidth = MIN_SIZE;
-        dstHeight = this.height * MIN_SIZE / this.width;
-      } else {
-        dstHeight = MIN_SIZE;
-        dstWidth = this.width * MIN_SIZE / this.height;
-      }
-      canvas.width = dstWidth;
-      canvas.height = dstHeight;
-      ctx.drawImage(this, 0, 0, this.width, this.height, 0, 0, dstWidth, dstHeight);
-      callback(canvas.toDataURL());
-    };
-    image.src = base64image;
-  };
+	    // ファイルが指定されていなければ何も起こらない
+	    if(!file || !blob) {
+	      return;
+	    }
 
-/*
-	$('input[name=photo]').change(function(e) {
-		var file = e.target.files[0];
-		if (file.type.match(/image.*/ /*)) {
-			var canvas = document.createElement("canvas"),
-					ctx = canvas.getContext('2d'),
-					image = new Image(),
-					size = 800;
-			canvas.width = canvas.height = 0;
-			image.src = URL.createObjectURL(file);
-			image.onload = function() {
-				var w = size, h = image.height * (size/image.width);
-				canvas.width = w;
-				canvas.height = h;
-				ctx.drawImage(image, 0, 0, w, h);
+	    var name, fd = new FormData();
+	    fd.append('file', blob); // ファイルを添付する
 
-				var img = document.createElement('img');
-				img.src = canvas.toDataURL(file.type);
+	    $.ajax({
+	      url: "updfile-result.php", // 送信先
+	      type: 'POST',
+	      dataType: 'json',
+	      data: fd,
+	      processData: false,
+	      contentType: false
+	    })
+	    .done(function( data, textStatus, jqXHR ) {
+	      // 送信成功
+	    })
+	    .fail(function( jqXHR, textStatus, errorThrown ) {
+	      // 送信失敗
+	    });
+			alert('送信しました');
 
-				var head = 'data:' + file.type + ';base64,';
-				var imgFileSize = Math.round((img.src.length - head.length)*3/4);
-				$('#contents').append("<div><span>" + "元画像サイズ" + (file.size/1024).toFixed(1) + "kb" + " => " + "変換後サイズ" + (imgFileSize/1024).toFixed(1) + "kb" + "</span></div>")
+	  });
 
-				$("#contents").append(img);
-			}
-		}
 	});
-*/
-
-  </script>
+	</script>
 
 </html>
